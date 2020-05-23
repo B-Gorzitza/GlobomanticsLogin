9895a3ff47c5b4145d8923e711354811bb6de023
Brad Gorzitza <FiveNinesReliable@gmail.com>
2020-05-23 14:07:39 -0600

    Add admin credentials

--- loginverification/src/main/java/fiveninesreliable/gmail/com/loginverification/LoginVerification.kt
+++ loginverification/src/main/java/fiveninesreliable/gmail/com/loginverification/LoginVerification.kt
@@ -2,7 +2,9 @@ package fiveninesreliable.gmail.com.loginverification
 
 class LoginVerification {
     fun verify(email: String, password: String): Boolean {
-        return email == "test@globomatics.com" &&
-                password == "test123"
+        return (email == "test@globomatics.com" &&
+                password == "test123") ||
+                (email == "admin@globomatics.com" &&
+                        password == "admin123")
     }
 }
