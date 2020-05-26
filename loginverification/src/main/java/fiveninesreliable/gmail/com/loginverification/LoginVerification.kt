d67babf973491ef5cab89d76a27a16a122c871a5
Brad Gorzitza <FiveNinesReliable@gmail.com>
2020-05-26 15:48:31 -0600

    Change admin password

--- loginverification/src/main/java/fiveninesreliable/gmail/com/loginverification/LoginVerification.kt
+++ loginverification/src/main/java/fiveninesreliable/gmail/com/loginverification/LoginVerification.kt
@@ -5,6 +5,6 @@ class LoginVerification {
         return (email == "test@globomatics.com" &&
                 password == "test123") ||
                 (email == "admin@globomatics.com" &&
-                        password == "admin123")
+                        password == "admin1234")
     }
 }
